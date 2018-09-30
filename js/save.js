78620cdd86ee0b67f213576159111020df2432a9
Masafuro <apa226@gmail.com>
2018-09-30 23:38:38 +0900

    保存機能を搭載
    残るは中身の整理となった。

---
+++ js/save.js
@@ -0,0 +1,52 @@
+(function() {
+  // <html> を clone
+  var html = document.getElementsByTagName('html')[0].cloneNode(true);
+
+  // href や src に指定されたURLを絶対パスに変換
+  /*
+  var nodes = html.querySelectorAll('[href],[src]');
+  for (var i=0, n=nodes.length; i<n; i++) {
+    if (nodes[i].href) { nodes[i].href=nodes[i].href; }
+    if (nodes[i].src) { nodes[i].src=nodes[i].src; }
+
+    console.log('href: ' + nodes[i].href + ', src: ' + nodes[i].src);
+  }
+  */
+
+  // ソースコードをテキストで取得
+  var src = html.innerHTML;
+  console.log(src.slice(0, 5000));
+
+  // 上記の src には DOCTYPE が含まれていないので別途用意
+  var name     = document.doctype.name;
+  var publicId = document.doctype.publicId;
+  var systemID = document.doctype.systemId;
+  var doctype  = '<!DOCTYPE ' + name
+                 + (publicId ? ' PUBLIC "' + publicId + '"' : '')
+                 + (systemID ? ' "' + systemID + '"' : '')
+                 + '>';
+  console.log(doctype);
+
+  // <html> タグを再構成
+  var htmlTag = '<html';
+  var attrs = html.attributes;
+  for (var i=0, n=attrs.length; i<n; i++) {
+    var attr = attrs[i];
+    htmlTag += ' ' + attr.nodeName + (attr.nodeValue ? '="' + attr.nodeValue + '"' : '');
+  }
+  htmlTag += '>';
+  console.log(htmlTag);
+
+  // ソースコードを Blob オブジェクトに変換してURLを取得
+  var blob    = new Blob([doctype, '\n', htmlTag, '\n', src, '\n</html>']);
+  var url     = window.URL || window.webkitURL;
+  var blobURL = url.createObjectURL(blob);
+
+  // <a> を新たに作成し、ダウンロード用の設定をいろいろ
+  var a = document.createElement('a');
+  // URI を元にダウンロード時のファイル名を決定
+  a.download = decodeURI(location.pathname+location.hash).replace(/\//g,'__').replace(/#/g,'--') + '.html';
+  a.href     = blobURL;
+
+  a.click();
+})();
--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 cd C:\Program Files (x86)\Google\Chrome\Application
-chrome.exe --allow-file-access-from-files --app=file:///C:/Users/masafuro/Desktop/LanguageCompiler/index.ja.html
+chrome.exe --allow-file-access-from-files file:///C:/Users/masafuro/Desktop/LanguageCompiler/index.ja.html
